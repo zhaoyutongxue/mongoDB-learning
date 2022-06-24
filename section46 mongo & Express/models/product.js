153008f3b35b7d437201aa6c6e1a27d2830c8c53
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-06-24 14:45:41 +1000

    462. 2 way referencing
    farm and product 2 way referencing

--- section46 mongo & Express/models/product.js
+++ section46 mongo & Express/models/product.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const Farm = require('./farm');
+const { Schema } = mongoose;
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -14,6 +16,10 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
 })
 
