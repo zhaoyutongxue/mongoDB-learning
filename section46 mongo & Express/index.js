50fc4f69028868ad4a17196c932272e0371ad70a
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-06-24 13:21:48 +1000

    restart the section

--- section46 mongo & Express/index.js
+++ section46 mongo & Express/index.js
@@ -4,12 +4,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 
+
 const Product = require('./models/product');
-const Farm = require('./models/farm')
-const categories = ['fruit', 'vegetable', 'dairy'];
 
-
-mongoose.connect('mongodb://localhost:27017/farmStandTake2', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
@@ -25,56 +23,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-// FARM ROUTES
-
-app.get('/farms', async (req, res) => {
-    const farms = await Farm.find({});
-    res.render('farms/index', { farms })
-})
-app.get('/farms/new', (req, res) => {
-    res.render('farms/new')
-})
-app.get('/farms/:id', async (req, res) => {
-    const farm = await Farm.findById(req.params.id).populate('products');
-    res.render('farms/show', { farm })
-})
-
-app.delete('/farms/:id', async (req, res) => {
-    const farm = await Farm.findByIdAndDelete(req.params.id);
-
-    res.redirect('/farms');
-})
-
-
-
-
-app.post('/farms', async (req, res) => {
-    const farm = new Farm(req.body);
-    await farm.save();
-    res.redirect('/farms')
-})
-
-app.get('/farms/:id/products/new', async (req, res) => {
-    const { id } = req.params;
-    const farm = await Farm.findById(id);
-    res.render('products/new', { categories, farm })
-})
-
-app.post('/farms/:id/products', async (req, res) => {
-    const { id } = req.params;
-    const farm = await Farm.findById(id);
-    const { name, price, category } = req.body;
-    const product = new Product({ name, price, category });
-    farm.products.push(product);
-    product.farm = farm;
-    await farm.save();
-    await product.save();
-    res.redirect(`/farms/${id}`)
-})
-
-
-
-// PRODUCT ROUTES
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
     const { category } = req.query;
@@ -99,7 +48,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id).populate('farm', 'name');
+    const product = await Product.findById(id)
     res.render('products/show', { product })
 })
 
@@ -126,6 +75,5 @@ app.delete('/products/:id', async (req, res) => {
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
 })
-
 
 
